3a3bf2f0d7b53c68dbf1e5281fb2ab0d5f4c951a
JazakVoda <blazin.steva@gmail.com>
2018-03-17 02:24:14 +0100

    Change

--- app.js
+++ app.js
@@ -2,39 +2,39 @@
     document.querySelector('.equally').addEventListener('click', function() {
         
         var num = document.getElementById('number').value;
-        if(num.includes('/')){
-            var operand = num.indexOf('/');
-            var num1 = num.substring(0, operand);
-            var num2 = num.substring(operand + 1);
-            
-            num = num1 / num2;
-            document.querySelector('.result').value = num;
-            
-        } else if(num.includes('*')){
-            var operand = num.indexOf('*');
-            num1 = num.substring(0, operand);
-            num2 = num.substring(operand + 1);
-            
-            num = num1 * num2;
-            document.querySelector('.result').value = num;
-            
-        } else if(num.includes('-')){
-            var operand = num.indexOf('-');
-            num1 = num.substring(0, operand);
-            num2 = num.substring(operand + 1);
-            
-            num = num1 - num2;
-            document.querySelector('.result').value = num;
-            
-        } else if(num.includes('+')){
-            var operand = num.indexOf('+');
-            num1 = num.substring(0, operand);
-            num2 = num.substring(operand + 1);
-            
-            num = num1 * 1 + num2 * 1;
-            document.querySelector('.result').value = num;
-        } else {
-            document.querySelector('.result').value = 'Try Again';
-        }
         
+        // Array of numbers numbers = ["26", "33", "56", "34", "23"]
+        var numbers = num.split(/\+|\-|\*|\//g);
+        
+        // Array of operators operators = ["+", "+", "-", "*", "/"]
+        var operators = num.replace(/[0-9]|\./g, "").split("");
+        
+        var divide = operators.indexOf("/");
+            while (divide != -1) {
+                numbers.splice(divide, 2, numbers[divide] / numbers[divide + 1]);
+                operators.splice(divide, 1);
+                divide = operators.indexOf("/");
+          }
+        
+        var multiply = operators.indexOf("*");
+            while (multiply != -1) {
+                numbers.splice(multiply, 2, numbers[multiply] * numbers[multiply + 1]);
+                operators.splice(multiply, 1);
+                multiply = operators.indexOf("*");
+          }
+        
+        var subtract = operators.indexOf("-");
+            while (subtract != -1) {
+                numbers.splice(subtract, 2, numbers[subtract] - numbers[subtract + 1]);
+                operators.splice(subtract, 1);
+                subtract = operators.indexOf("-");
+          }
+        
+        var add = operators.indexOf("+");
+            while (add != -1) {
+                numbers.splice(add, 2, parseFloat(numbers[add]) + parseFloat(numbers[add + 1]));
+                operators.splice(add, 1);
+                add = operators.indexOf("+");
+          }
+        document.querySelector('.result').value = num;
     });
